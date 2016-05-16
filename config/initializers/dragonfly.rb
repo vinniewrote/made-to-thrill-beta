8531eaf66c4fb27d7881a2c557b83ba0210eb95f
Vizente Freeman <vizente@leo8eleven.com>
2016-05-15 21:47:38 -0500

    initial commit

---
+++ config/initializers/dragonfly.rb
@@ -0,0 +1,22 @@
+# Configure
+Dragonfly.app(:engine).configure do
+  plugin :imagemagick,
+    convert_command:  `which convert`.strip.presence || '/usr/local/bin/convert',
+    identify_command: `which identify`.strip.presence || '/usr/local/bin/identify'
+
+  verify_urls true
+
+  secret 'e1459f690011aa1d09cd77561b455312e29a007a6b87f119a1f4f64e3ef0804a'
+
+  url_format '/images/dynamic/:job/:sha/:basename.:ext'
+
+  fetch_file_whitelist /public/
+
+  fetch_url_whitelist /.+/
+end
+
+# Logger
+Dragonfly.logger = Rails.logger
+
+# Mount as middleware
+Rails.application.middleware.use Dragonfly::Middleware, :engine
